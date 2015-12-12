44bb4ee3b4f04d2c9e86af2860c424f170a66906
zarco07 <zarco07@hotmail.com>
2015-12-11 23:36:59 -0600

    Segundo commit

--- miniReto1.playground/Contents.swift
+++ miniReto1.playground/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+for i in 0...100 {
+    if i >= 30 && i <= 40 {
+        print("#\(i)\t Viva Swift!!!")
+    }
+    else if (i % 5) == 0 && i > 0 {
+        print("#\(i)\t Bingo!!!")
+    }
+    else if (i % 2) == 0 && i > 0 {
+        print("#\(i)\t par!!!")
+    }
+    else if (i % 2) >  0 && i > 0 {
+        print("#\(i)\t inpar!!!")
+    }
+    
+    else{
+    print("#\(i)")
+    }
+}
+
+
